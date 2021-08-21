5aeb44c2eb747df269321dcb46388b958bc3bd0e
Yogesh Kumar <yogiky2002@gmail.com>
2021-08-21 13:57:10 +0530

    movie-search app created using react with omdb api

---
+++ Day 3/movie-search/src/App.js
@@ -0,0 +1,90 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+// https://omdbapi.com/?t=the avengers&apikey=995c09b7 
+
+function App() {
+  // state variable -- movie
+  let [movieinfo, setMovieinfo] = useState(null);
+  let [title, setTitle] = useState("iron man 2");
+
+  useEffect(() => {
+    getMovieData();
+
+  }, [])
+
+  function readTitle(value) {
+    // console.log(value);
+    setTitle(value);
+  }
+
+  function getMovieData() {
+    let url = `https://omdbapi.com/?t=${title}&apikey=995c09b7`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((movie) => {
+        console.log(movie);
+        setMovieinfo(movie);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  return (
+    <div className="app">
+
+      <div className="container">
+        <div className="padd">
+          <h1>Movie Search</h1>
+        </div>
+        <div className="input-group">
+          <input type="text" placeholder="Enter Movie Name" onChange={(event) => { readTitle(event.target.value) }} className="search-field" />
+          <button className="btn" onClick={getMovieData}>Get Movie</button>
+        </div>
+        {
+          movieinfo?.Error === undefined ? (
+
+            <div className="movie">
+              <div className="poster">
+                <img src={movieinfo?.Poster} alt="poster" className="img-poster" />
+              </div>
+              <div className="details">
+                <div className="pd">
+                  <h1>{movieinfo?.Title}</h1>
+                  <p><strong>Genre</strong> : {movieinfo?.Genre}</p>
+                  <p><strong>Directed By</strong> :{movieinfo?.Director}</p>
+                  <p><strong>Plot</strong> :{movieinfo?.Plot}</p>
+                  <p><strong>Cast</strong> :{movieinfo?.Actors}</p>
+                  <p><strong>Box Office</strong> :{movieinfo?.BoxOffice}</p>
+                  <p><strong>Language</strong> :{movieinfo?.Language}</p>
+                  <p><strong>Release Date</strong> :{movieinfo?.Released}</p>
+                  <p><strong>Runtime</strong> :{movieinfo?.Runtime}</p>
+
+                  <div className="ratings">
+                    {
+                      movieinfo?.Ratings.map((rating, index) => (
+
+                        <div key={index}>
+                          <strong>{rating.Source}</strong>
+                          <h3>{rating.Value}</h3>
+                        </div>
+                      ))
+                    }
+                  </div>
+
+                </div>
+              </div>
+
+            </div>
+          ) :
+            (
+              <h1>Movie not found</h1>
+            )
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
